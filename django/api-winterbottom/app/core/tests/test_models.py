3cba687d40aaa60574ac852f89d660de0ab43a28
Guillermo Brachetta <brachetta@me.com>
2021-01-22 18:30:07 +0100

    Add Winterbottom API

---
+++ django/api-winterbottom/app/core/tests/test_models.py
@@ -0,0 +1,79 @@
+from unittest.mock import patch
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .. import models
+
+
+def sample_user(email="test@test.com", password="testpass"):
+    """Create a sample user"""
+
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@londonappdev.com"
+        password = "Password123"
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized"""
+        email = "test@BRACHETTA.com"
+        user = get_user_model().objects.create_user(email, "123asd")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "123")
+
+    def test_create_new_superuser(self):
+        """Test creating superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@test.com", "test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+
+        tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(), name="Cucumber"
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title="Stake and mushroom sauce",
+            time_minutes=5,
+            price=5.00,
+        )
+        self.assertEqual(str(recipe), recipe.title)
+
+    @patch("uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test the image is saved in the correct location"""
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "myimage.jpg")
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
