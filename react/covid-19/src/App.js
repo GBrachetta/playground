b51eee9084200ab681cc153e6590a7fa1daf7ea4
Guillermo Brachetta <brachetta@me.com>
2021-01-23 13:58:33 +0100

    Display changes and styled components

--- react/covid-19/src/App.js
+++ react/covid-19/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import coronaImage from './images/image.png';
 
 class App extends React.Component {
   state = {
     data: {},
+    country: '',
   };
 
   async componentDidMount() {
@@ -14,14 +16,20 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="corona" />
         <Cards data={data} />
-        <Chart />
-        <CountryPicker />
+        <Chart data={data} country={country} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
       </div>
     );
   }
